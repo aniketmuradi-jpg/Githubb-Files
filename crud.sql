1eb02b9d38d8510f03d7804ae7fe6b8ed75fab95
ANIKET <aniketmuradi@gmail.com>
2026-01-22 11:40:41 +0530

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employees
+( id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), salary DECIMAL(10,2), origin VARCHAR(255));
+-- employee_id of employee PREMSHANKAR= '24680'
+-- employee_id of employee ANIKET= '26802'
+-- employee_name= PREMSHANKAR  VARCHAR(50)
+-- employee_name= ANIKET  VARCHAR(50)
+-- employee_salary DECIMAL(8000000,100000000) of PREMSHANKAR
+-- employee_salary DECIMAL(100000000,400000000) of ANIKET
+
+-- READ TABLE employees(id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), salary DECIMAL(10,2) origin VARCHAR(255))
+--  salary DECIMAL(10,2))
+
+-- UPDATE TABLE employees(id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), salary DECIMAL(10,2) origin VARCHAR(255))
+-- employees(id AUTO_INCREMENT PRIMARY KEY)
+
+-- DELETE TABLE employees(id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), salary DECIMAL(10,2), origin VARCHAR(255))
+-- employee origin VARCHAR(255)
+
+ 
